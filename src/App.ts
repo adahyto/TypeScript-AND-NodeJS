e70d938bc73e9c677c2db76393973c919568b268
Adam <adahy.to@gmail.com>
2018-05-05 22:13:21 +0200

    Add files via upload

---
+++ src/App.ts
@@ -0,0 +1,30 @@
+import * as express from 'express'
+
+class App {
+  public express
+
+  constructor(){
+    this.express = express()
+    this.mountRoutes()
+  }
+
+  private mountRoutes (): void {
+    const router = express.Router()
+    router.get('/', (req, res)=>{
+      res.json({
+        name: 'NodeJS&TypeScript',
+        message: 'enjoy'
+      })
+    })
+    router.post('/', (req, res)=>{
+      res.json({
+        req: 'POST',
+        status: 'working'
+      })
+    })
+
+    this.express.use('/', router)
+  }
+}
+
+export default new App().express
